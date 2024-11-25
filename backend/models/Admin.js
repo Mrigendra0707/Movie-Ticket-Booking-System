421639fe8293b970dfd47c950c0be6c443e5d502
VISHAL KUMAR <123532074+vishalhcf@users.noreply.github.com>
2024-11-26 00:03:06 +0530

    Update Admin.js

--- backend/models/Admin.js
+++ backend/models/Admin.js
@@ -1,23 +1,31 @@
 import mongoose from "mongoose";
 
-const adminSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
+const bookingSchema = new mongoose.Schema({
+  movie: {
+    type: mongoose.Types.ObjectId,
+    ref: "Movie",
     required: true,
   },
-  password: {
-    type: String,
+  date: {
+    type: Date,
     required: true,
-    minLength: 6,
   },
-  addedMovies: [
-    {
-      type: mongoose.Types.ObjectId,
-      ref:"Movie",
-      
+  seatNumber: {
+    type: String, // Changed to String to match "A1", "B1", etc.
+    required: true,
+    validate: {
+      validator: function (seat) {
+        // Regex to validate alphanumeric seat numbers (e.g., A1, B10)
+        return /^[A-Z]\d{1,2}$/.test(seat);
+      },
+      message: (props) => ${props.value} is not a valid seat number!,
     },
-  ],
+  },
+  user: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
 });
 
-export default mongoose.model("Admin", adminSchema);
+export default mongoose.model("Booking", bookingSchema);
