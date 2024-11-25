6e3fedf0a43e6db3dcb60795f762dc8d7ac05ffd
Mrigendra0707 <123158605+Mrigendra0707@users.noreply.github.com>
2024-11-26 00:09:33 +0530

    Update Movie.js

--- backend/models/Movie.js
+++ backend/models/Movie.js
@@ -1,17 +1,18 @@
-
-import mongoose from "mongoose";
-
-const Schema = mongoose.Schema;
-
-const movieSchema = new Schema({
+const movieSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: { type: String, required: true },
   releaseDate: { type: Date, required: true },
   posterUrl: { type: String, required: true },
-  trailerUrl: { type: String }, // Add this line for the trailer URL
+  trailerUrl: { type: String }, 
   featured: { type: Boolean, required: true },
   actors: [{ type: String, required: true }],
   admin: { type: mongoose.Types.ObjectId, ref: "Admin", required: true },
+  seatLayout: [
+    {
+      number: { type: Number, required: true },
+      booked: { type: Boolean, required: true },
+    },
+  ],
 });
 
 export default mongoose.model("Movie", movieSchema);
